b03ab4cf7973bd8f2ff87157cd13dc6ee9f40e43
guokairong <992926186@qq.com>
2021-04-08 15:50:46 +0800

    字符串操作

--- DataType/str_op.py
+++ DataType/str_op.py
@@ -1,3 +1,12 @@
-str_a = r"abc234123434!23\n@#$@#$#$%$^#$"
-str_b = "1232131232131"
-print(str_a+str_b)
+# 去除转义——r
+# str_a = r"abc234123434!23\n@#$@#$#$%$^#$"
+# str_b = "1232131232131"
+# print(str_a+str_b)
+
+var = "dsfdgfdg"
+# 索引
+print(var[1])
+# 切片，前闭后开原则
+print(var[1:])
+# start:stop:step
+print(var[1:5:3])
