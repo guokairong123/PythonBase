df199196bdcd6b2d53b870463040d4c8519c4e7d
guokairong <992926186@qq.com>
2021-04-09 11:36:10 +0800

    字面量

---
+++ In_Out/Literal_demo.py
@@ -0,0 +1,14 @@
+name = 'ldsfd'
+age = 20
+list1 = [1, 3, 4]
+dic1 = {'name': 'tom', 'gender': 'man'}
+namelist = ['Tom', 'Mary', "Mike"]
+# print('my name is {0}, age is {1}, {0}{1}{0}'.format(name, age))
+# print('my list is {}, dict is {}'.format(list1, dic1))
+#
+# print('my friends name is {}、{} and {}'.format(*namelist))
+#
+# print('my name is {name}, gender is {gender}'.format(**dic1))
+print(f"my name is {name}")
+
+print(f"the data is {(lambda x:x+1)(2)}")
